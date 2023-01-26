6b8abb2ad2aae4a6f4f0b7f5911d465e92a0a6bb
Jonathan Leeming <jpleeming51@gmail.com>
2023-01-26 00:46:07 +0000

    Added basic JSX-like support

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,5 +1,6 @@
-use rocket::*;
+use rocket::{*, http::ContentType};
 use rocket_dyn_templates::{Template, context};
+use yew::{prelude::*, ServerRenderer};
 
 use super::RouteProvider;
 
@@ -7,6 +8,28 @@ use super::RouteProvider;
 #[get("/")]
 fn index() -> Template {
     Template::render("index", context!{title: "LU CSS"})
+}
+
+#[function_component(JSX)]
+fn jsx_component() -> Html {
+    let component = html! { <div id="component">{"Some component"}</div> };
+    
+    let text_var = "You can interpolate text variables";
+    
+    html! {
+       <div>
+          {"You can type text right into the elements"}
+          { component }
+          { text_var }
+       </div>
+    }
+}
+
+#[get("/jsx")]
+async fn jsx() -> (ContentType, String) {
+    let mut buff = String::new();
+    ServerRenderer::<JSX>::new().render_to_string(&mut buff).await;
+    (ContentType::HTML, buff)
 }
 
 
@@ -18,6 +41,6 @@ impl RouteProvider for HomeRoutes {
     }
 
     fn routes() -> Vec<Route> {
-        routes![index]
+        routes![index, jsx]
     }
 }
