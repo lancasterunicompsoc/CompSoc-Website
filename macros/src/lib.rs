006fb48025eeb1543d4a179ddea0eefbdbca4765
Jonathan Leeming <jpleeming51@gmail.com>
2023-01-26 14:03:23 +0000

    Added `router` macro to simplfy route creation

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::{TokenStream, TokenTree};
-use quote::{quote, format_ident};
+use quote::{quote, format_ident, ToTokens};
 use syn;
 
 #[proc_macro_attribute]
@@ -36,5 +36,51 @@ pub fn component_route(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
 
+    generated.into()
+}
+
+
+#[proc_macro_attribute]
+pub fn router(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut iter = attr.into_iter();
+    let raw_base_path = iter.next().expect("No path was provided").to_string();
+    let mut raw_base_path_iter = raw_base_path.chars();
+    raw_base_path_iter.next();
+    let base_path = raw_base_path_iter.take(raw_base_path.len() - 2).collect::<String>();
+
+    let module: syn::ItemMod = syn::parse(item).unwrap();
+    let (_, items) = &module.content.expect("A router must define one or more routes");
+    let route_handler_names: Vec<_> = items.iter()
+        .filter_map(|item| match item {
+            syn::Item::Fn(f) => Some(f),
+            _ => None
+        })
+        .map(|f| &f.sig.ident)
+        .collect();
+    
+    let router_name = &module.ident;
+
+    let module_contents: Vec<_> = items.iter()
+        .filter(|item| match item {
+            syn::Item::Use(_) => false,
+            _ => true,
+        })
+        .map(|item| item.to_token_stream()).collect();
+
+    let generated = quote!{
+        #(#module_contents)*
+
+        pub struct #router_name;
+
+        impl RouteProvider for #router_name {
+            fn base_url() -> &'static str {
+                #base_path
+            }
+
+            fn routes() -> Vec<Route> {
+                routes![#(#route_handler_names,)*]
+            }
+        }
+    };
     generated.into()
 }
--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,39 +1,28 @@
-use rocket::{*, http::ContentType};
-use rocket_dyn_templates::{Template, context};
+use rocket::{http::ContentType, *};
+use rocket_dyn_templates::{context, Template};
 
-use super::RouteProvider;
-use super::super::engines::rsx::*;
+use crate::engines::rsx::*;
+use super::{RouteProvider, router};
 
-
-#[get("/")]
-fn index() -> Template {
-    Template::render("index", context!{title: "LU CSS"})
-}
-
-
-#[component_route(get, "/jsx", JSX)]
-fn jsx() -> Html {
-    let component = html! { <div id="component">{"Some component"}</div> };
-    
-    let text_var = "You can interpolate text variables";
-    
-    html! {
-       <div>
-          {"You can type text right into the elements"}
-          { component }
-          { text_var }
-       </div>
-    }
-}
-
-pub struct HomeRoutes;
-
-impl RouteProvider for HomeRoutes {
-    fn base_url() -> &'static str {
-        "/"
+#[router("/")]
+mod HomeRoutes {
+    #[get("/")]
+    fn index() -> Template {
+        Template::render("index", context! {title: "LU CSS"})
     }
 
-    fn routes() -> Vec<Route> {
-        routes![index, jsx]
+    #[component_route(get, "/jsx", JSX)]
+    fn jsx() -> Html {
+        let component = html! { <div id="component">{"Some component"}</div> };
+
+        let text_var = "You can interpolate text variables";
+
+        html! {
+           <div>
+              {"You can type text right into the elements"}
+              { component }
+              { text_var }
+           </div>
+        }
     }
 }
