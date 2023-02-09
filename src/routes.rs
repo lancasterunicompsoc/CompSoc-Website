250d420c7b98be3138cae1086e633211de7d33c6
Jonathan Leeming <jpleeming51@gmail.com>
2023-02-09 23:48:15 +0000

    Begun work on reloading scss in debug mode

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,9 @@
 mod home;
 
+use std::path::{Path, PathBuf};
+
 pub use macros::router;
-use rocket::{*, fs::FileServer};
+use rocket::{*, fs::FileServer, request::FromRequest};
 
 use home::HomeRoutes;
 
@@ -21,6 +23,7 @@ pub trait RouteManager {
 impl RouteManager for Rocket<Build> {
     fn add_routes(self) -> Self {
         self.add_route_provider::<HomeRoutes>()
+            .mount("/public", routes![styles])
             .mount("/public", FileServer::from("public/"))
     }
 
@@ -28,3 +31,41 @@ impl RouteManager for Rocket<Build> {
         self.mount(Provider::base_url(), Provider::routes())
     }
 }
+
+
+struct CSSGuard;
+
+#[derive(Debug)]
+enum CSSGuardError {}
+
+#[async_trait]
+impl<'r> FromRequest<'r> for CSSGuard {
+    type Error = CSSGuardError;
+
+    #[cfg(debug_assertions)]
+    async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+        let path = Path::new(req.uri().path().as_str());
+        match path.extension() {
+            Some(extension) => {
+                if extension != "css" {
+                    return request::Outcome::Forward(());
+                }
+            },
+            None => return request::Outcome::Forward(())
+        };
+        request::Outcome::Success(CSSGuard {})
+    }
+
+    #[cfg(not(debug_assertions))]
+    async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+        request::Outcome::Forward(())
+    }
+}
+
+
+#[get("/<path..>")]
+fn styles(path: PathBuf, _css: CSSGuard) {
+    let file_path = Path::new("public/").join(path);
+    println!("Recompiling {:?}", file_path);
+}
+
