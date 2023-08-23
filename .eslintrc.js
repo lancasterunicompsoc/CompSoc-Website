37da2956bc0f2ef7c4882cfac2f34ec6c7059c87
Jonathan Leeming <jonathan@leeming.dev>
2023-08-23 16:28:09 +0100

    added Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
-
 module.exports = {
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-  ],
+  extends: ["@nuxtjs/eslint-config-typescript"],
+  env: {
+    browser: true,
+    node: true,
+  },
   rules: {
+    // Style
     semi: ["error", "always"],
     quotes: ["error", "double"],
+    "arrow-parens": "off",
+    "comma-dangle": "off",
+    "space-before-function-paren": "off",
+
+    // Quality
+    "no-use-before-define": "warn",
+    "require-await": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 };
-
