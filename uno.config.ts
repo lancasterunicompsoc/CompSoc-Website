5c0766cad8c4bc47294308adad36400f4708ca95
Jonathan Leeming <jonathan@leeming.dev>
2023-09-07 08:50:22 +0100

    corrected `btn` colours

--- uno.config.ts
+++ uno.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   shortcuts: [
     [
       "btn",
-      "px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50",
+      "px-4 py-1 rounded inline-block bg-highlight1Light dark:bg-highlight1Dark text-white cursor-pointer hover:bg-highlight2Light dark:hover:bg-highlight2Dark disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50",
     ],
     [
       "btn-secondary",
