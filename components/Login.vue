d15a33248a0efff13fe51efe5c5966ba9b2e451d
hugohabicht01 <anon@example.org>
2023-10-01 14:57:19 +0100

    add correct login url

--- components/Login.vue
+++ components/Login.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 const { isLoggedIn, logOut } = useAuth();
+const config = useRuntimeConfig();
+const { loginUrl } = config.public;
 </script>
 
 <template>
   <ClientOnly>
     <div v-bind="$attrs">
-      <a
-        v-if="!isLoggedIn"
-        class="pointer-cursor"
-        href="https://weblogin.lancs.ac.uk/jwt/compsoctestdev"
-      >
-        Log In
-      </a>
+      <a v-if="!isLoggedIn" class="pointer-cursor" :href="loginUrl"> Log In </a>
       <a v-else href="#" class="pointer-cursor" @click="logOut">Log Out</a>
     </div>
   </ClientOnly>
