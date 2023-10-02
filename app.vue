4ff75c55b686f876d34bca65f2d3d3f017bbdc7a
Jonathan Leeming <jonathan@leeming.dev>
2023-10-02 20:13:02 +0100

    merge

--- app.vue
+++ app.vue
@@ -66,3 +66,4 @@ main a:not([class]) {
   }
 }
 </style>
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
+<<<<<<< Updated upstream
   <div class="page">
     <Header />
     <slot />
     <Footer />
   </div>
 </template>
+
+<style>
+    /* Sticky footer, globally */
+    main {
+        flex: 1;
+    }
+</style>
+
