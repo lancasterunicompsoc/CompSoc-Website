cb5520c339a0ef109ee783f5b4ac6353ebdfe2f9
hugohabicht01 <39224742+hugohabicht01@users.noreply.github.com>
2023-10-08 11:58:12 +0000

    fix auth button not updating

--- components/Login.vue
+++ components/Login.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { useAuthStore } from "~/stores/auth";
 
-const { isLoggedIn, logOut } = useAuthStore();
+const authStore = useAuthStore();
+
 const config = useRuntimeConfig();
 const { loginUrl } = config.public;
 </script>
@@ -9,8 +10,8 @@ const { loginUrl } = config.public;
 <template>
   <ClientOnly>
     <div v-bind="$attrs">
-      <a v-if="!isLoggedIn" class="pointer-cursor" :href="loginUrl">Log In</a>
-      <a v-else href="#" class="pointer-cursor" @click="logOut">Log Out</a>
+      <a v-if="!authStore.isLoggedIn" class="pointer-cursor" :href="loginUrl">Log In</a>
+      <a v-else href="#" class="pointer-cursor" @click="authStore.logOut">Log Out</a>
     </div>
   </ClientOnly>
 </template>
