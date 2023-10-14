43fee07c397f2ccef2c24abaee8463bd52138df6
Jonathan Leeming <jonathan@leeming.dev>
2023-10-14 12:26:41 +0100

    upgraded string magic to array magic

--- components/Terminal/commands/utils.ts
+++ components/Terminal/commands/utils.ts
@@ -6,23 +6,22 @@ import type { EventType } from "~/components/events/utils";
 const eventToSlug = (event: EventType) => `${event.id}-${event.name.toLowerCase().split(" ").join("-")}`;
 
 export function eventToFile(event: EventType): Entry {
-  let content = `# ${event.name} [${event.difficulty}]
-
-${unixAnySpan(event.unixStartTime, event.unixEndTime)}
-Location: ${event.location}
-Organizer: ${event.organizer}
-`;
-  if (event.slides) {
-    content += `${event.slides}\n`;
-  }
-  content += "\n";
-  content += event.summary;
-  content += "\n\n";
-  content += event.description;
-  content += "\n";
+  const content = [
+    `# ${event.name} [${event.difficulty}]`,
+    "",
+    `${unixAnySpan(event.unixStartTime, event.unixEndTime)}`,
+    `Location: ${event.location}`,
+    `Organizer: ${event.organizer}`,
+    event.slides ? `Slides: ${event.slides}\n` : null,
+    "",
+    event.summary,
+    "",
+    event.description,
+    "",
+  ];
   return {
     type: EntryType.file,
     name: eventToSlug(event),
-    content,
+    content: content.filter(line => line !== null).join("\n"),
   };
 }
