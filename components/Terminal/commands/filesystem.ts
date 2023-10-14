b8fe090caa4589eaba1e9af7d1f049312bbfcfc3
Jonathan Leeming <jonathan@leeming.dev>
2023-10-14 12:31:10 +0100

    improved union

--- components/Terminal/commands/filesystem.ts
+++ components/Terminal/commands/filesystem.ts
@@ -9,16 +9,18 @@ export enum EntryType {
 }
 
 export interface DirEntry {
+  type: EntryType.directory,
   name: string;
   children: ChildFactory;
 }
 
 export interface FileEntry {
+  type: EntryType.file,
   name: string;
   content: string;
 }
 
-export type Entry = (DirEntry & { type: EntryType.directory }) | (FileEntry & { type: EntryType.file });
+export type Entry = DirEntry | FileEntry;
 
 type ChildFactory = (state: State) => Entry[];
 
