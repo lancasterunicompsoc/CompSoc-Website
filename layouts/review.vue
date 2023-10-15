0f04c3b647ef9d9e04c263f4461ac63980bde423
Jonathan Leeming <jonathan@leeming.dev>
2023-10-15 23:15:55 +0100

    fixed layout

---
+++ layouts/review.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page flex flex-column">
+    <Header />
+    <div class="py-4 grow flex flex-column">
+      <slot />
+    </div>
+    <Footer />
+  </div>
+</template>
--- pages/events/review/[id].vue
+++ pages/events/review/[id].vue
@@ -2,6 +2,10 @@
 import Star from "~/components/SVG/Star";
 import StarFilled from "~/components/SVG/StarFilled";
 
+definePageMeta({
+  layout: "review",
+});
+
 // TODO: proper animation between states
 
 const score = ref<number>(0);
@@ -25,32 +29,74 @@ function ghostAway() {
 </script>
 
 <template>
-  <form>
-    <input type="hidden" :value="score" />
-    <div class="row">
-      <button
-        v-for="x in [1, 2, 3, 4, 5]"
-        :key="x"
-        type="button"
-        class="star fill-yellow"
-        @click="() => { score = x; }"
-        @mouseenter="() => { hovered = true; ghostScore = x; }"
-        @mouseleave="() => { hovered = false; useTimeoutFn(ghostAway, 125); }
-        "
-      >
-        <StarFilled v-if="ghostScore > 0 ? ghostScore >= x : score >= x" />
-        <Star v-else />
-      </button>
-    </div>
+  <main>
+    <form>
+      <input type="hidden" name="score" :value="score" />
+      <div class="row">
+        <button
+          v-for="x in [1, 2, 3, 4, 5]"
+          :key="x"
+          type="button"
+          class="star fill-yellow light:stroke-darkgrey"
+          @click="
+            () => {
+              score = x;
+            }
+          "
+          @mouseenter="
+            () => {
+              hovered = true;
+              ghostScore = x;
+            }
+          "
+          @mouseleave="
+            () => {
+              hovered = false;
+              useTimeoutFn(ghostAway, 125);
+            }
+          "
+        >
+          <StarFilled v-if="ghostScore > 0 ? ghostScore >= x : score >= x" />
+          <Star v-else />
+        </button>
+      </div>
 
-    <button type="submit">
-      {{ submitMessage }}
-    </button>
-  </form>
+      <div class="message-wrapper">
+        <label for="message">Feedback (optional)</label>
+        <textarea
+          id="message"
+          name="message"
+          class="grow block px-4 py-2 bg-transparent outline-none border border-highlight1Light dark:border-highlight1Dark"
+        ></textarea>
+      </div>
+
+      <div class="row">
+        <button type="submit" class="btn btn-primary text-5xl p-6">
+          {{ submitMessage }}
+        </button>
+      </div>
+    </form>
+  </main>
 </template>
 
 <style scoped>
+main {
+  display: flex;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+}
 .star {
   width: 15vw;
+}
+.message-wrapper {
+  width: 75%;
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 }
 </style>
