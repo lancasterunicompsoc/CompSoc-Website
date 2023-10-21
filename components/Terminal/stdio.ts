09c3646eb7c7a45587a3fdc2f463120f642e55fe
Jonathan Leeming <jonathan@leeming.dev>
2023-10-21 13:21:54 +0100

    converted switch to lookup table

--- components/Terminal/stdio.ts
+++ components/Terminal/stdio.ts
@@ -32,10 +32,10 @@ enum Underline {
 }
 
 type Style = {
-  foreground: Color,
-  background: Color,
-  weight: Weight,
-  underline: Underline,
+  foreground: Color;
+  background: Color;
+  weight: Weight;
+  underline: Underline;
 };
 const DEFAULT_STYLE: Style = {
   foreground: Color.white,
@@ -47,64 +47,42 @@ const DEFAULT_STYLE: Style = {
 export type StyledSpan = { text: string; style: Style };
 
 function parseEscapeCode(code: string, currentStyle: Style): Style {
-  return code.split(";").reduce((style, part) => {
-    switch (Number.parseInt(part)) {
-      case 0:
-        return DEFAULT_STYLE;
-      case 1:
-        return { ...style, weight: Weight.bold };
-      case 2:
-        return { ...style, weight: Weight.faint };
-      case 4:
-        return { ...style, underline: Underline.single };
-      case 7:
-        return { ...style, foreground: style.background, background: style.foreground };
-      case 21:
-        return { ...style, underline: Underline.double };
-      case 22:
-        return { ...style, weight: Weight.normal };
-      case 24:
-        return { ...style, underline: Underline.none };
-      case 30:
-        return { ...style, foreground: Color.black };
-      case 31:
-        return { ...style, foreground: Color.red };
-      case 32:
-        return { ...style, foreground: Color.green };
-      case 33:
-        return { ...style, foreground: Color.yellow };
-      case 34:
-        return { ...style, foreground: Color.blue };
-      case 35:
-        return { ...style, foreground: Color.purple };
-      case 36:
-        return { ...style, foreground: Color.cyan };
-      case 37:
-        return { ...style, foreground: Color.white };
-      case 39:
-        return { ...style, foreground: DEFAULT_STYLE.foreground };
-      case 40:
-        return { ...style, background: Color.black };
-      case 41:
-        return { ...style, background: Color.red };
-      case 42:
-        return { ...style, background: Color.green };
-      case 43:
-        return { ...style, background: Color.yellow };
-      case 44:
-        return { ...style, background: Color.blue };
-      case 45:
-        return { ...style, background: Color.purple };
-      case 46:
-        return { ...style, background: Color.cyan };
-      case 47:
-        return { ...style, background: Color.white };
-      case 49:
-        return { ...style, background: DEFAULT_STYLE.background };
-      default:
-        throw new Error("invalid code");
-    }
-  }, currentStyle);
+  return code.split(";").reduce(
+    (style, part) =>
+      ({
+        0: DEFAULT_STYLE,
+        1: { ...style, weight: Weight.bold },
+        2: { ...style, weight: Weight.faint },
+        4: { ...style, underline: Underline.single },
+        7: {
+          ...style,
+          foreground: style.background,
+          background: style.foreground,
+        },
+        21: { ...style, underline: Underline.double },
+        22: { ...style, weight: Weight.normal },
+        24: { ...style, underline: Underline.none },
+        30: { ...style, foreground: Color.black },
+        31: { ...style, foreground: Color.red },
+        32: { ...style, foreground: Color.green },
+        33: { ...style, foreground: Color.yellow },
+        34: { ...style, foreground: Color.blue },
+        35: { ...style, foreground: Color.purple },
+        36: { ...style, foreground: Color.cyan },
+        37: { ...style, foreground: Color.white },
+        39: { ...style, foreground: DEFAULT_STYLE.foreground },
+        40: { ...style, background: Color.black },
+        41: { ...style, background: Color.red },
+        42: { ...style, background: Color.green },
+        43: { ...style, background: Color.yellow },
+        44: { ...style, background: Color.blue },
+        45: { ...style, background: Color.purple },
+        46: { ...style, background: Color.cyan },
+        47: { ...style, background: Color.white },
+        49: { ...style, background: DEFAULT_STYLE.background },
+      })[Number.parseInt(part)] ?? style,
+    currentStyle,
+  );
 }
 
 export function colorize(input: string): StyledSpan[] {
