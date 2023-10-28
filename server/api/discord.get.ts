86f1ec94c2e021d3c9c155605be6faa5fe1293fd
lancasterunicompsoc <138233536+lancasterunicompsoc@users.noreply.github.com>
2023-10-29 00:35:55 +0100

    fix auth validation

--- server/api/discord.get.ts
+++ server/api/discord.get.ts
@@ -2,7 +2,7 @@ import { dateToUnix, unixToDate, unixAnySpan } from "~/utils/time";
 
 export default defineEventHandler(async event => {
   const config = useRuntimeConfig();
-  if (event.headers.get("authorization") !== process.env.CRON_SECRET) {
+  if (event.headers.get("authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
     return { ok: false };
   }
 
