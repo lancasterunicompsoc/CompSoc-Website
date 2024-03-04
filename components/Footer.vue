3786175b67bf8a8d891226faf43ee86096e76b03
Jonathan Leeming <jonathan@leeming.dev>
2024-03-04 12:38:54 +0000

    added decrees link

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,35 +5,42 @@ import Instagram from "./SVG/Instagram.vue";
 </script>
 <template>
   <footer
-    class="bg-#ddd dark:bg-lightgrey p-4 flex m-0 items-center flex-row justify-center"
+    class="bg-#ddd dark:bg-lightgrey p-4 m-0 flex items-center flex-col justify-center"
   >
-    <a
-      href="https://discord.gg/compsoc "
-      target="_blank"
-      class="social-icon"
-      aria-label="Discord"
-    >
-      <Discord />
-    </a>
-    <a
-      href="https://github.com/LUCompSoc"
-      target="_blank"
-      class="social-icon"
-      aria-label="Github"
-    >
-      <Github />
-    </a>
-    <a
-      href="https://www.instagram.com/lucompsoc"
-      target="_blank"
-      class="social-icon"
-      aria-label="Instagram"
-    >
-      <Instagram />
-    </a>
-    <a href="mailto:info@compsoc.io" aria-label="email" class="email">
-      info@compsoc.io
-    </a>
+    <div class="flex m-0 mb-2 items-center flex-row justify-center">
+      <a
+        href="https://discord.gg/compsoc"
+        target="_blank"
+        class="social-icon"
+        aria-label="Discord"
+      >
+        <Discord />
+      </a>
+      <a
+        href="https://github.com/LUCompSoc"
+        target="_blank"
+        class="social-icon"
+        aria-label="Github"
+      >
+        <Github />
+      </a>
+      <a
+        href="https://www.instagram.com/lucompsoc"
+        target="_blank"
+        class="social-icon"
+        aria-label="Instagram"
+      >
+        <Instagram />
+      </a>
+      <a href="mailto:info@compsoc.io" aria-label="email" class="email">
+        info@compsoc.io
+      </a>
+    </div>
+    <div class="text-#888 flex m-0 items-center flex-row justify-center">
+      <a href="https://decrees.docs.compsoc.io" target="_blank">
+        Presidential Decrees
+      </a>
+    </div>
   </footer>
 </template>
 
