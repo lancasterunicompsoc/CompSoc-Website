2c18a857909df8afc1322c58067b657d7bdb53cc
Isaac - The456 <the456@the456gamer.dev>
2024-08-20 19:24:57 +0100

    remove clippy, move terminal

---
+++ pages/terminal.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const TermComponent = defineAsyncComponent(
+  () => import("~/components/Terminal/index.vue"),
+);
+</script>
+
+<template>
+  <div class="center">
+    <NuxtLink to="/">&lt;- Back to Home</NuxtLink>
+    <Suspense>
+      <ClientOnly>
+        <TermComponent />
+      </ClientOnly>
+    </Suspense>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
